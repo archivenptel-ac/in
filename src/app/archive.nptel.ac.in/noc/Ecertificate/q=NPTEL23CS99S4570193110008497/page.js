d2f57d291ad23feb8bb6c2950ab0af1b42379c00
archivenptel-ac <gordgedew@gmail.com>
2024-09-21 22:37:10 +0530

    Update page.js

--- src/app/archive.nptel.ac.in/noc/Ecertificate/q=NPTEL23CS99S4570193110008497/page.js
+++ src/app/archive.nptel.ac.in/noc/Ecertificate/q=NPTEL23CS99S4570193110008497/page.js
@@ -2,7 +2,8 @@ export default function Page() {
     return (
       <div style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
         <a
-         href="./content/noc/NOC24/SEM1/Ecertificates/107/noc24-cs99/Course/NPTEL23CS99S4570193110008497.pdf"
+         //href="./content/noc/NOC24/SEM1/Ecertificates/107/noc24-cs99/Course/NPTEL23CS99S4570193110008497.pdf"
+
           target="_blank"
           style={{
             color: 'white',
